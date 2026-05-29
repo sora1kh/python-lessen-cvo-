dd16ea94a69fc3d5b7d3157a7096d966dc2ad9b6
sora1kh <georgestudybe@gmail.com>
2026-05-29 13:29:35 +0200

    202605291329

---
+++ les2_oefB.py
@@ -0,0 +1,6 @@
+voornaam = input("Wat is jouw voornaam? ")
+leeftijd = int(input("Wat is jouw leeftijd? "))
+
+print("Hallo", voornaam + "!")
+print("Volgend jaar ben je", leeftijd + 1, "jaar oud.")
+print(voornaam.upper())
