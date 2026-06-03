4a6072818fd8a09218b675992da9c35bec9a01eb
sora1kh <georgestudybe@gmail.com>
2026-06-03 20:48:47 +0200

    factuur uitbreiding klaar

--- les2_factuur.py
+++ les2_factuur.py
@@ -1,18 +1,27 @@
 klant = input("Naam van de klant: ")
 
 aantal = int(input("Aantal producten: "))
-prijs = float(input("Prijs per stuk: "))
 
-subtotaal = aantal * prijs
-btw = subtotaal * 0.21
-totaal = subtotaal + btw
+if aantal < 0:
+    print("Fout: aantal producten mag niet negatief zijn.")
+else:
+    prijs = float(input("Prijs per stuk: "))
+    btw_percentage = float(input("BTW-percentage (6, 12 of 21): "))
 
-print("==============================")
-print("Factuur voor:", klant)
-print("Aantal producten:", aantal)
-print("Prijs per stuk: €", prijs)
-print("------------------------------")
-print("Subtotaal: €", subtotaal)
-print("BTW (21%): €", btw)
-print("TOTAAL: €", totaal)
-print("==============================")
+    subtotaal = aantal * prijs
+    btw = subtotaal * (btw_percentage / 100)
+    totaal = subtotaal + btw
+
+    subtotaal = round(subtotaal, 2)
+    btw = round(btw, 2)
+    totaal = round(totaal, 2)
+
+    print("==============================")
+    print("Factuur voor:", klant)
+    print("Aantal producten:", aantal)
+    print("Prijs per stuk: €", prijs)
+    print("------------------------------")
+    print("Subtotaal: €", subtotaal)
+    print("BTW (" + str(btw_percentage) + "%): €", btw)
+    print("TOTAAL: €", totaal)
+    print("==============================")
