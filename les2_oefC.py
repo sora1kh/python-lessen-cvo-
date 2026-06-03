78104ccd444f11ed17c3a455d5ac10122cf3d573
sora1kh <georgestudybe@gmail.com>
2026-06-03 19:43:51 +0200

    oefC deel1 klaar

---
+++ les2_oefC.py
@@ -0,0 +1,12 @@
+getal1 = int(input("Geef het eerste getal: "))
+getal2 = int(input("Geef het tweede getal: "))
+
+som = getal1 + getal2
+verschil = getal1 - getal2
+product = getal1 * getal2
+deling = getal1 / getal2
+
+print("Som:", som)
+print("Verschil:", verschil)
+print("Product:", product)
+print("Deling:", deling)
